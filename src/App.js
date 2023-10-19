656b827b48cb3635abc79ddb51dddd212beb47de
Vialoha <vi62469@gmail.com>
2023-10-19 15:59:46 +0100

    websiteencostas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap CSS
+import 'font-awesome/css/font-awesome.min.css'; // Import FontAwesome icons
+import {useTranslation} from "react-i18next";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { t } = useTranslation();
+
+  return <h2>{t('Welcome to React')}</h2>;
 }
 
 export default App;
